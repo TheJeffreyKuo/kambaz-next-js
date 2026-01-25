d2137d9184cc07d5a93dae6200380ae3347df539
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-01-24 22:24:08 -0500

    Update page.tsx

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -3,9 +3,9 @@ export default function Signin() {
  return (
    <div id="wd-signin-screen">
      <h3>Sign in</h3>
-     <input placeholder="username" className="wd-username" /> <br />
-     <input placeholder="password" type="password" className="wd-password" /> <br />
-     <Link href="profile" id="wd-signin-btn"> Sign in </Link> <br />
-     <Link href="signup" id="wd-signup-link"> Sign up </Link>
+     <input className="wd-username" placeholder="username" /> <br />
+     <input className="wd-password" placeholder="password" type="password" /> <br />
+     <Link id="wd-signin-btn" href="/dashboard"> Sign in </Link> <br />
+     <Link id="wd-signup-link" href="signup"> Sign up </Link>
    </div>
 );}
