64f07b9a2fa094a6da582408c2a882e1835eb462
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-01-25 00:09:22 -0500

    Update layout.tsx

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,14 +1,8 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
-
-export default async function CoursesLayout({
-  children,
-  params,
-}: {
-  children: ReactNode;
-  params: { cid: string };
-}) {
-  const { cid } = params;
+export default async function CoursesLayout(
+  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
+ const { cid } = await params;
  return (
    <div id="wd-courses">
      <h2>Courses {cid}</h2>
