d0e1fe9cb8a3d4cbc0db610a5028d8176c88c393
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-02-06 22:30:21 -0500

    Update page.tsx

--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -3,8 +3,8 @@ export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" type="text" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
+      <input placeholder="username" type="text" className="wd-username" defaultValue="alice"/><br/>
+      <input placeholder="password" type="password" className="wd-password" defaultValue="password123"/><br/>
       <input placeholder="verify password"
              type="password" className="wd-password-verify" /><br/>
       <Link  href="Profile" > Sign up </Link><br />
