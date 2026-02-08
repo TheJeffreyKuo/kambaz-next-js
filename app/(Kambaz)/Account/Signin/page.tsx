a16667a6bba7ee8a9896fb3243880c91300cd3d2
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-02-08 03:57:01 -0500

    Update page.tsx

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -1,21 +1,25 @@
 import Link from "next/link";
+import { FormControl } from "react-bootstrap";
+
 export default function Signin() {
- return (
-   <div id="wd-signin-screen">
-     <h3>Sign in</h3>
-     <input
-        className="wd-username"
+  return (
+    <div id="wd-signin-screen" className="p-4" style={{ maxWidth: "400px" }}>
+      <h1 className="mb-4">Sign in</h1>
+     <FormControl
+        id="wd-username"
+        className="mb-2"
         placeholder="username"
         type="text"
         defaultValue="Johndoe"
-      /><br />
-     <input
-        className="wd-password"
+      />
+     <FormControl
+        id="wd-password"
+        className="mb-2"
         placeholder="password"
         type="password"
         defaultValue="dontlookhere"
-      /><br />
-     <Link id="wd-signin-btn" href="/Dashboard"> Sign in </Link> <br />
-     <Link id="wd-signup-link" href="Signup"> Sign up </Link>
-   </div>
+      />
+      <Link id="wd-signin-btn" className="btn btn-primary w-100 mb-2"href="/Dashboard"> Sign in </Link> <br />
+      <Link id="wd-signup-link" className="text-primary text-decoration-underline" href="/Account/Signup"> Sign up </Link>
+    </div>
 );}
