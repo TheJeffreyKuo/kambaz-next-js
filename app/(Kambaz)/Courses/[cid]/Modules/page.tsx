93b83cb4b70f89ff80277d110f0f5666705c1ef1
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-02-08 01:31:15 -0500

    Update page.tsx

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -11,7 +11,6 @@ export default function Modules() {
         <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-secondary">
             <BsGripVertical className="me-2 fs-3" /> Week 1
-            <ModulesControls />
           </div>
           <ListGroup className="wd-lessons rounded-0">
             <ListGroupItem className="wd-lesson p-3 ps-1">
@@ -32,7 +31,6 @@ export default function Modules() {
         <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-secondary">
             <BsGripVertical className="me-2 fs-3" /> Week 2
-            <ModulesControls />
           </div>
           <ListGroup className="wd-lessons rounded-0">
             <ListGroupItem className="wd-lesson p-3 ps-1">
