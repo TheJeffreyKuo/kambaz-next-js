c879814f44863c62962c70169c1f0bc8f26c468b
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-02-08 02:47:12 -0500

    Update page.tsx

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -1,67 +1,91 @@
+import {
+  Form,
+  FormLabel,
+  FormControl,
+  FormSelect,
+  FormCheck,
+  Button,
+} from "react-bootstrap";
+
 export default function AssignmentEditor() {
   return (
-    <div id="wd-assignments-editor">
-      <label htmlFor="wd-name">Assignment Name</label>
-      <input id="wd-name" defaultValue="A1 - ENV + HTML" /><br /><br />
-      <textarea id="wd-description">
-        The assignment is available online Submit a link to the landing page of
-      </textarea>
+    <Form id="wd-assignments-editor">
+      <FormLabel htmlFor="wd-name">Assignment Name</FormLabel>
+      <FormControl id="wd-name" defaultValue="A1" /><br /><br />
+      <FormControl
+            id="wd-description"
+            as="textarea"
+            rows={9}
+            className="border"
+            defaultValue={`The assignment is available online. 
+Submit a link to the landing page of your Web application running on Netlify.
+
+• Your full name and section
+• Links to each of the lab assignments
+• Link to the Kanbas application
+• Links to all relevant source code repositories
+
+The Kanbas application should include a link to navigate back to the landing page.`}
+          />
       <br />
       <table>
         <tr>
           <td align="right" valign="top">
-            <label htmlFor="wd-points">Points</label>
+            <FormLabel htmlFor="wd-points">Points</FormLabel>
           </td>
           <td>
-            <input id="wd-points" type="number" defaultValue={100} />
+            <FormControl id="wd-points" type="number" defaultValue={100} />
           </td>
         </tr>
         <tr>
           <td align="right" valign="top">
-            <label htmlFor="wd-group">Assignment Group</label>
+            <FormLabel htmlFor="wd-group">Assignment Group</FormLabel>
           </td>
           <td>
-            <select id="wd-group">
-                <option selected value="assignments">ASSIGNMENTS</option>
-            </select>
+            <FormSelect id="wd-group" defaultValue="assignments">
+                <option value="assignments">ASSIGNMENTS</option>
+                <option value="QUIZZES">QUIZZES</option>
+                <option value="EXAMS">EXAMS</option>
+                <option value="PROJECT">PROJECT</option>
+            </FormSelect>
           </td>
         </tr>
         <tr>
           <td align="right" valign="top">
-            <label htmlFor="wd-display-grade-as">Display Grade as</label>
+            <FormLabel htmlFor="wd-display-grade-as">Display Grade as</FormLabel>
           </td>
           <td>
-            <select id="wd-display-grade-as">
-                <option selected value={"percentage"}>Percentage</option>
-            </select>
+            <FormSelect id="wd-display-grade-as" defaultValue={"percentage"}>
+                <option value={"percentage"}>Percentage</option>
+                <option value="Points">Points</option>
+                <option value="Letter">Letter</option>
+            </FormSelect>
           </td>
         </tr>
          <tr>
           <td align="right" valign="top">
-            <label htmlFor="wd-submission-type">Submission Type</label>
+            <FormLabel htmlFor="wd-submission-type">Submission Type</FormLabel>
           </td>
           <td>
-            <select id="wd-submission-type">
-                <option selected value={"online"}>Online</option>
-            </select>
+            <FormSelect id="wd-submission-type" defaultValue={"online"}>
+                <option value={"online"}>Online</option>
+                <option value="On Paper">On Paper</option>
+                <option value="External Tool">External Tool</option>
+                <option value="No Submission">No Submission</option>
+            </FormSelect>
             <table>
                 <tr>
                     <td>
-                        Online Entry Options
+                        <FormLabel className="fw-bold small">Online Entry Options</FormLabel>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <input type="checkbox" name="check-text" id="wd-text-entry"/>
-                        <label htmlFor="wd-text-entry">Text Entry</label><br/>
-                        <input type="checkbox" name="check-website" id="wd-website-url"/>
-                        <label htmlFor="wd-website-url">Website URL</label><br/>
-                        <input type="checkbox" name="check-media" id="wd-media-recordings"/>
-                        <label htmlFor="wd-media-recordings">Media Recordings</label><br/>
-                        <input type="checkbox" name="check-annotation" id="wd-student-annotation"/>
-                        <label htmlFor="wd-student-annotation">Student Annotation</label><br/>
-                        <input type="checkbox" name="check-file" id="wd-file-upload"/>
-                        <label htmlFor="wd-file-upload">File Uploads</label><br/>
+                        <FormCheck type="checkbox" name="check-text" id="wd-text-entry" label="Text Entry" /><br/>
+                        <FormCheck type="checkbox" name="check-website" id="wd-website-url" label="Website URL" defaultChecked/><br/>
+                        <FormCheck type="checkbox" name="check-media" id="wd-media-recordings" label="Media Recordings" /><br/>
+                        <FormCheck type="checkbox" name="check-annotation" id="wd-student-annotation" label="Student Annotation" /><br/>
+                        <FormCheck type="checkbox" name="check-file" id="wd-file-upload" label="File Uploads" /><br/>
                     </td>
                 </tr>
             </table>
@@ -74,40 +98,40 @@ export default function AssignmentEditor() {
           <td>
             <table>
                 <tr>
-                    <td><label htmlFor="wd-assign-to">Assign to</label></td>
+                    <td><FormLabel htmlFor="wd-assign-to" className="fw-bold small">Assign to</FormLabel></td>
                 </tr>
                 <tr>
                     <td>
-                        <input id="wd-assign-to" defaultValue={"Everyone"} />
+                        <FormControl id="wd-assign-to" defaultValue={"Everyone"} />
                     </td>
                 </tr>
                 <tr>
-                    <td><label htmlFor="wd-due-date"> Due </label></td>
+                    <td><FormLabel htmlFor="wd-due-date" className="fw-bold small"> Due </FormLabel></td>
                 </tr>
                 <tr>
                     <td>
-                        <input type="date"
-                       value="2024-05-13"
+                      <FormControl type="date"
+                       defaultValue="2024-05-13"
                        id="wd-due-date"/><br/>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <label htmlFor="wd-available-from"> Available from </label>
+                      <FormLabel htmlFor="wd-available-from" className="fw-bold small"> Available from </FormLabel>
                     </td>
                     <td>
-                        <label htmlFor="wd-available-until"> Until </label>
+                      <FormLabel htmlFor="wd-available-until" className="fw-bold small"> Until </FormLabel>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <input type="date"
-                       value="2024-05-06"
+                      <FormControl type="date"
+                       defaultValue="2024-05-06"
                        id="wd-available-from"/><br/>
                     </td>
                     <td>
-                        <input type="date"
-                       value="2024-05-20"
+                      <FormControl type="date"
+                       defaultValue="2024-05-20"
                        id="wd-available-until"/><br/>
                     </td>
                 </tr>
@@ -119,9 +143,9 @@ export default function AssignmentEditor() {
         </tr>
         <tr>
           <td colSpan={2} align="right">
-            <button type="button" id="wd-cancel">Cancel</button> <button type="button" id="wd-save">Save</button>
+            <Button type="button" id="wd-cancel">Cancel</Button> <Button type="button" id="wd-save">Save</Button>
           </td>
         </tr>
       </table>
-    </div>
+    </Form>
 );}
