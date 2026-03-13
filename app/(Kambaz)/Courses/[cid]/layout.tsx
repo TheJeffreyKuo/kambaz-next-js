5b1e70fb5a048994bb2a478b43e446e83b93f934
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-03-13 00:27:46 -0400

    Update layout.tsx

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,26 +1,30 @@
-import { ReactNode } from "react";
+"use client";
+import { ReactNode, useState } from "react";
 import CourseNavigation from "./Navigation";
-import CourseBreadcrumb from "./Breadcrumb";
 import { FaAlignJustify } from "react-icons/fa6";
-import { courses } from "../../../Database";
-
-export default async function CoursesLayout(
-  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
- const { cid } = await params;
- const course = courses.find((c) => c._id === cid);
- return (
-  <div id="wd-courses">
-    <h2 className="text-danger">
-        <FaAlignJustify className="me-4 fs-4 mb-1" />
-        {course?.name} </h2>
-    <CourseBreadcrumb course={course} />
-    <hr />
-    <div className="d-flex">
-      <div className="d-none d-md-block">
-        <CourseNavigation />
+import { useSelector } from "react-redux";
+import { useParams } from "next/navigation";
+import { RootState } from "../../store";
+export default function CoursesLayout({ children }: { children: ReactNode }) {
+  const { cid } = useParams();
+  const { courses } = useSelector((state: RootState) => state.coursesReducer);
+  const course = courses.find((course: any) => course._id === cid);
+  const [showNav, setShowNav] = useState(true);
+  return (
+    <div id="wd-courses">
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" onClick={() => setShowNav(!showNav)} />
+        {course?.name}
+      </h2>
+      <hr />
+      <div className="d-flex">
+        {showNav && (
+          <div className="d-none d-md-block">
+            <CourseNavigation />
+          </div>
+        )}
+        <div className="flex-fill">{children}</div>
       </div>
-      <div className="flex-fill">
-        {children}
-      </div></div>
-  </div>
-);}
+    </div>
+  );
+}
