8749b53c01a27bf13443013a9d70d28d5f8b9a92
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-03-13 00:27:34 -0400

    Update ModulesList.tsx

--- app/(Kambaz)/Courses/[cid]/Modules/ModulesList.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/ModulesList.tsx
@@ -1,23 +1,23 @@
+"use client";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
-import ModulesControls from "./modulesControls";
 import { BsGripVertical } from "react-icons/bs";
 import LessonControlButtons from "./LessonControlButtons";
-import * as db from "../../../../Database";
-
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store";
 export default function ModulesList({ cid }: { cid: string }) {
-  const courseModules = db.modules.filter((m) => m.course === cid);
+  const { modules } = useSelector((state: RootState) => state.modulesReducer);
+  const courseModules = modules.filter((m: any) => m.course === cid);
   return (
     <div>
-      <ModulesControls /><br /><br /><br /><br />
       <ListGroup className="rounded-0" id="wd-modules">
-        {courseModules.map((module) => (
+        {courseModules.map((module: any) => (
           <ListGroupItem key={module._id} className="wd-module p-0 mb-5 fs-5 border-gray">
             <div className="wd-title p-3 ps-2 bg-secondary">
               <BsGripVertical className="me-2 fs-3" /> {module.name}
             </div>
             {module.lessons && (
               <ListGroup className="wd-lessons rounded-0">
-                {module.lessons.map((lesson) => (
+                {module.lessons.map((lesson: any) => (
                   <ListGroupItem key={lesson._id} className="wd-lesson p-3 ps-1">
                     <BsGripVertical className="me-2 fs-3" /> {lesson.name}
                     <LessonControlButtons />
