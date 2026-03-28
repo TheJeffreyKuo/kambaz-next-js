98a16af3591309d80af5832bf3f9a98342a1598a
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-03-27 23:48:13 -0400

    Create client.ts

---
+++ app/(Kambaz)/Enrollments/client.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+const axiosWithCredentials = axios.create({ withCredentials: true });
+const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+const ENROLLMENTS_API = `${HTTP_SERVER}/api/enrollments`;
+export const enrollInCourse = async (courseId: string) => {
+  const response = await axiosWithCredentials.post(ENROLLMENTS_API, { courseId });
+  return response.data;
+};
+export const unenrollFromCourse = async (courseId: string) => {
+  const response = await axiosWithCredentials.delete(ENROLLMENTS_API, { data: { courseId } });
+  return response.data;
+};
+export const findMyEnrollments = async () => {
+  const { data } = await axiosWithCredentials.get(ENROLLMENTS_API);
+  return data;
+};
