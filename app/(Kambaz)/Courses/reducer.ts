bf89810bc0b5330217f5e08acc75f16125403406
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-03-27 23:48:08 -0400

    Update reducer.ts

--- app/(Kambaz)/Courses/reducer.ts
+++ app/(Kambaz)/Courses/reducer.ts
@@ -1,16 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { courses } from "../../Database";
-import { v4 as uuidv4 } from "uuid";
 const initialState = {
-  courses: courses,
+  courses: [] as any[],
 };
 const coursesSlice = createSlice({
   name: "courses",
   initialState,
   reducers: {
     addNewCourse: (state, { payload: course }) => {
-      const newCourse = { ...course, _id: uuidv4() };
-      state.courses = [...state.courses, newCourse] as any;
+      state.courses = [...state.courses, course] as any;
     },
     deleteCourse: (state, { payload: courseId }) => {
       state.courses = state.courses.filter(
